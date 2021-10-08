9bc9c514ce1fc99b861e6bc789fec2dc4bfc04e2
Acostam331 <acostam331@gmail.com>
2021-10-07 23:38:39 -0600

    Update index.js

--- react_course/src/index.js
+++ react_course/src/index.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import reactDom from 'react-dom';
+import React from "react";
+import reactDom from "react-dom";
 
 // You can use <React.Fragment> or <>
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <h3>Hello people</h3>
-      <ul>
-        <li>
-          <a href="#">Hello World</a>
-        </li>
-        <img src="" alt="" />
-        <input type="text" />
-      </ul>
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-reactDom.render(<Greeting />, document.getElementById('root'));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images.unsplash.com/photo-1633326259678-fec951d6a797?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1480&q=80"
+    alt=""
+    width="200px"
+  />
+);
+
+const Title = () => <h1>I love you to the moon and back</h1>;
+
+const Author = () => <h4>Amelia Hepworth</h4>;
+
+reactDom.render(<BookList />, document.getElementById("root"));
