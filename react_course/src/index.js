ba46cc23f930c160aa41b9f96f9890c8358247f0
Acostam331 <acostam331@gmail.com>
2021-10-10 13:11:40 -0600

    course

--- react_course/src/index.js
+++ react_course/src/index.js
@@ -1,41 +1,78 @@
 import React from "react";
 import reactDom from "react-dom";
+//css
+import "./index.css";
 
 // You can use <React.Fragment> or <>
+// variables
+const books = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1633326259678-fec951d6a797?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1480&q=80",
+    title: "I love you to the moon and back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1633114073758-c4be9aeb15ac?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    title: "Our class is a family",
+    author: "Shannon Olsen",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1633801695482-f3a98465b84b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1287&q=80",
+    title: "The Vanishing Half: A Novel",
+    author: "Brit Bennet",
+  },
+];
 
 function BookList() {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
+
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("hello World");
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" width="200px" height="200px" />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      <button type="button" onClick={clickHandler}>
+        Reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More complex example
+      </button>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images.unsplash.com/photo-1633326259678-fec951d6a797?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1480&q=80"
-    alt=""
-    width="200px"
-  />
-);
-
-const Title = () => <h1>I love you to the moon and back</h1>;
-
-const Author = () => <h4>Amelia Hepworth</h4>;
-
 reactDom.render(<BookList />, document.getElementById("root"));
+
+{
+  /* <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      > */
+}
