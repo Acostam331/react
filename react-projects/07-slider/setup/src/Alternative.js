205269ffc51d0ceb781f1c7ad8b6cc03ba6791ef
Acostam331 <acostam331@gmail.com>
2021-10-19 22:40:16 -0600

    course

--- react-projects/07-slider/setup/src/Alternative.js
+++ react-projects/07-slider/setup/src/Alternative.js
@@ -7,18 +7,41 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex);
-    } else if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, people]);
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+
+      if (index > people.length - 1) {
+        index = 0;
+      }
+
+      return index;
+    });
+  };
+
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+
+      if (index < 0) {
+        index = people.length - 1;
+      }
+
+      return index;
+    });
+  };
 
   useEffect(() => {
     let slider = setInterval(() => {
-      setIndex(index + 1);
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+
+        if (index > people.length - 1) {
+          index = 0;
+        }
+
+        return index;
+      });
     }, 3000);
 
     return () => clearInterval(slider);
@@ -55,36 +78,12 @@ function App() {
             </article>
           );
         })}
-        <button className="prev" onClick={() => setIndex(index - 1)}>
+        <button className="prev" onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={() => setIndex(index + 1)}>
+        <button className="next" onClick={nextSlide}>
           <FiChevronRight />
         </button>
-        {/* <button
-          className="prev"
-          onClick={() => {
-            if (index === 0) {
-              setIndex(people.length - 1);
-            } else {
-              setIndex(index - 1);
-            }
-          }}
-        >
-          <FiChevronLeft />
-        </button>
-        <button
-          className="next"
-          onClick={() => {
-            if (index === people.length - 1) {
-              setIndex(0);
-            } else {
-              setIndex(index + 1);
-            }
-          }}
-        >
-          <FiChevronRight />
-        </button> */}
       </div>
     </section>
   );
