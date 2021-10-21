ede7f12eb34802d6e2629ce4383f8a4497950078
Acostam331 <acostam331@gmail.com>
2021-10-21 17:24:44 -0600

    course and projects

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
 // import Final from './tutorial/2-useEffect/final/3-useEffect-fetch-data';
 
 function App() {
