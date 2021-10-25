a303c95625dcb23abfe3d47eaed140ba344ff8fe
Acostam331 <acostam331@gmail.com>
2021-10-24 23:23:03 -0600

    course and project

--- react-projects/10-grocery-bud/setup/src/App.js
+++ react-projects/10-grocery-bud/setup/src/App.js
@@ -1,9 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      showAlert(true, 'please enter value', 'danger');
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      //show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
+  const showAlert = (show = false, msg = '', type = '') => {
+    setAlert({ show: show, type, msg });
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
